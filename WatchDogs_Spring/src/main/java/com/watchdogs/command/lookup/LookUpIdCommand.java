bdab38b7de6e25c8b3285cb02ec80d3357c07de6
ltsme <guswns2415@naver.com>
2021-08-20 02:41:58 +0900

    Spring

---
+++ WatchDogs_Spring/src/main/java/com/watchdogs/command/lookup/LookUpIdCommand.java
@@ -0,0 +1,46 @@
+package com.watchdogs.command.lookup;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.ui.Model;
+
+import com.watchdogs.command.home.BCommand_new;
+import com.watchdogs.dao.DaoLookupId;
+import com.watchdogs.dao.LoginCheckDao;
+import com.watchdogs.dto.DtoAdmin;
+import com.watchdogs.dto.DtoUser;
+
+public class LookUpIdCommand implements BCommand_new {
+
+	@Override
+	public void execute(SqlSession sqlSession, Model model, HttpSession httpSession) {
+		// TODO Auto-generated method stub
+
+		Map<String, Object> map = model.asMap();
+		HttpServletRequest request = (HttpServletRequest) map.get("request");
+		
+		String userEmail = request.getParameter("useremail");
+		String userName = request.getParameter("username");
+		System.out.println("2_1");
+		
+		DaoLookupId daolookupid = sqlSession.getMapper(DaoLookupId.class);
+		ArrayList<DtoUser> dtoUser = daolookupid.lookupiddao(userEmail, userName);
+
+		System.out.println("2_2");
+		if(dtoUser.size() == 0) { // 아이디 찾기 실패
+			httpSession.setAttribute("result", 0);
+			
+		}else { // 아이디 찾기 성공
+			httpSession.setAttribute("result", 1);
+			httpSession.setAttribute("userid", dtoUser.get(0).getUserId());
+		}
+		
+	}
+
+}
